dedf210ebf0eea4338fb3f23ba65a79df1372594
dfm88 <diegof.university@gmail.com>
2021-01-17 10:34:35 +0100

    first

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D _rigidB;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        _rigidB.velocity = new Vector2(horizontalInput, _rigidB.velocity.y);
+    }
+}
